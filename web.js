a33ac90e8db3cd72ef593495e4ea6cf2dae07d8a
Sangwoo Han <sangwoo.han00@gmail.com>
2014-08-25 00:48:35 +0900

    modified web.js

--- web.js
+++ web.js
@@ -6,8 +6,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('./index.html');
-  response.send(buf.toString())
+  response.send(fs.readFileSync('./index.html').toString())
 })
 
 app.listen(app.get('port'), function() {
